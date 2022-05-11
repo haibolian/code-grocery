beee43362b670634305a279007a75adbaec94033
lianhaibo <957377783@qq.com>
2022-05-11 15:26:35 +0800

    简易版手写 diff 完毕

--- vnode-diff/src/index.js
+++ vnode-diff/src/index.js
@@ -4,26 +4,29 @@ import patch from './patch';
 
 const container = document.getElementById('app')
 
-const demo1 = h('ul', { key: 'ul' }, [
+const demo1 = h('ul', {}, [
   h('li', { key: 'A' }, 'A'),
   h('li', { key: 'B' }, 'B'),
-  // h('li', { key: 'Q' }, 'Q'),
   h('li', { key: 'C' }, 'C'),
   h('li', { key: 'D' }, 'D'),
+  h('li', { key: 'E' }, 'E')
 ])
 
-const demo2 = h('ul', { key: 'ul' }, [
-  h('li', { key: 'Q' }, 'Q5'),
-  h('li', { key: 'P' }, 'P5'),
-  h('li', { key: 'D' }, 'D3'),
-  h('li', { key: 'C' }, 'C2'),
-  h('li', { key: 'A' }, 'A0'),
-  h('li', { key: 'B' }, 'B1'),
+const demo2 = h('ul', {}, [
+  h('li', { key: 'QQ' }, 'QQB'),
+  h('li', { key: 'C' }, 'C'),
+  h('li', { key: 'D' }, 'D'),
+  h('li', { key: 'B' }, 'B'),
+  h('li', { key: 'F' }, 'F'),
+  h('li', { key: 'G' }, 'G')
 ])
+
+let oldVnode
 
 patch(container, demo1)
-
+oldVnode = demo1
 const btn = document.getElementById('btn')
 btn.addEventListener('click', () => {
-  patch(demo1, demo2)
+  patch(oldVnode, demo2)
+  oldVnode = demo2
 })
